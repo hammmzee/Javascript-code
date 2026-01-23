a17a565a92103586948bdd0ad8fe7aaf4cbb73e9
hammmzee <162350824+hammmzee@users.noreply.github.com>
2026-01-23 23:24:08 +0500

    Add files via upload

---
+++ code.js
@@ -0,0 +1,6 @@
+let x = 3;
+let y = ++x; // x becomes 4 here in 2nd line
+
+// x value which is 4 went in y variable and y becomes 4, also x becomes 4 because of x++
+
+console.log("the value of x is: " + x, "The value of y is: " + y);
