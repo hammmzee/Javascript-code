b10ed42c24e1a808073260bc313a3a02dcc8c4af
hammmzee <162350824+hammmzee@users.noreply.github.com>
2026-09-06 18:45:32 +0500

    Improve BMI calculation validation and messages

--- BMI Calculator/BMI.js
+++ BMI Calculator/BMI.js
@@ -1,19 +1,24 @@
-const button = document.querySelector('.Calculate-Button')
+const button = document.querySelector('.Calculate-Button');
 
 button.addEventListener('click', () => {
 
     const height = document.querySelector('#height').value;
     const weight = document.querySelector('#weight').value;
-    const result = document.querySelector('#result')
+    const result = document.querySelector('#result');
 
+    if (height === '' && weight === '') {
+        result.value = 'Enter values';
 
-    if(height === '' || isNaN(Number(height)) || height < 0) {
-        result.value = "Invalid height"
-    } else if (weight  === '' || isNaN(Number(weight)) || weight < 0){
-        result.value = "Invalid Weight"
+    } else if (height === '' || isNaN(Number(height)) || Number(height) <= 0) {
+        result.value = 'Invalid height';
+
+    } else if (weight === '' || isNaN(Number(weight)) || Number(weight) <= 0) {
+        result.value = 'Invalid weight';
+
     } else {
         const BMI = (Number(weight) / (Number(height) * Number(height))) * 10000;
-        result.value = BMI.toFixed(2)
+
+        result.value = BMI.toFixed(2);
     }
 });
 
